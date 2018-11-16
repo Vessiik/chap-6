22a1f1a6f8d3d430f3f84ab856f67c6c501c2ef5
Vessiik <44364703+Vessiik@users.noreply.github.com>
2018-11-16 12:00:19 +0200

    Add files via upload

---
+++ chap 6/task 13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace task_13 //problem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter binary number: ");
+            int n = Int32.Parse(Console.ReadLine());
+            string toDecimal = Convert.ToString(Convert.ToInt32 (n, 2), 10);
+            string toDecimal = NewMethod(0);
+            Console.WriteLine("Result is {0}", toDecimal);
+        }
+        private static string NewMethod(int n)
+        {
+           
+
+        }
+}
+}
